36b1a898053c44b468a439baf0d702e7eacead9b
mnaofficialbd <mna.official92@gmail.com>
2025-08-15 20:50:40 +0600

    custom-button component update

--- app/components/Shared/StylistCustomButton.tsx
+++ app/components/Shared/StylistCustomButton.tsx
@@ -33,11 +33,11 @@ const StylistCustomButton = ({
             className="absolute inset-0"
             style={{
               background:
-                "conic-gradient(from var(--angle), transparent 25%, #06b6d4, transparent 50%)",
+                "conic-gradient(from var(--angle), transparent 25%, red, transparent 80%)",
               animation: "shimmer-spin 2.5s linear infinite",
             }}
           />
-          <span className="relative z-10 inline-flex items-center justify-center w-full h-full px-5 py-2 text-gray-900 dark:text-white bg-white dark:bg-gray-900 rounded-full group-hover:bg-gray-100 dark:group-hover:bg-gray-800 transition-colors duration-300">
+          <span className="relative z-10 inline-flex items-center justify-center w-full h-full px-5 py-2 text-gray-900 dark:text-white bg-white dark:bg-gradient-to-r from-blue-700 to-blue-400 rounded-full group-hover:bg-gray-100 dark:group-hover:bg-gray-800 transition-colors duration-300">
             {buttonText}
           </span>
         </button>
